2b4789d837b42230e67e906c14d6d1732d79e8f6
WonJoong <realjoong@gmail.com>
2020-08-07 18:05:35 +0900

    Added filter screen of CBNU and changed some design

--- app/src/main/java/com/example/menupan/Schools/ChungBuk/main_chungbuk.java
+++ app/src/main/java/com/example/menupan/Schools/ChungBuk/main_chungbuk.java
@@ -1,8 +1,11 @@
 package com.example.menupan.Schools.ChungBuk;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class main_chungbuk extends AppCompatActivity {
 
     private List<String> autoCompleteTextList;
+    private View filterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +25,37 @@ public class main_chungbuk extends AppCompatActivity {
         setContentView(R.layout.main_chungbuk_layout);
 
         autoCompleteTextList = new ArrayList<String>();//리스트 생성
+        Button button_cbnu_filter = (Button) findViewById(R.id.cbnu_filter);
+        Button button_cbnu_back = (Button) findViewById(R.id.cbnu_option_back);
+        filterView = (LinearLayout) findViewById(R.id.cbnu_linearlayout_filter);
 
         settingList();
 
-        final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
+        final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
 
         //AutoCompleteTextView에 어댑터 연결
         autoCompleteTextView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, autoCompleteTextList));
+
+        /*필터 버튼 클릭시 필터 보여주는 코드*/
+        button_cbnu_filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFilter();
+            }
+        });
+
+        /*뒤로가기 버튼 클릭시 필터 layout다시 가리는 코드*/
+        button_cbnu_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideFilter();
+            }
+        });
     }
 
     /*autoCompleteTextlist의 리스트에 데이터 추가하는 부분
-    * 순서는 기존 메뉴판 res.layout에 있는 순서대로 넣다가 중간에 멈춤*/
-    private void settingList(){
+     * 순서는 기존 메뉴판 res.layout에 있는 순서대로 넣다가 중간에 멈춤*/
+    private void settingList() {
         autoCompleteTextList.add("브리또인");
         autoCompleteTextList.add("은화수식당");
         autoCompleteTextList.add("블루리프");
@@ -44,5 +67,16 @@ public class main_chungbuk extends AppCompatActivity {
         autoCompleteTextList.add("뚱글이 순대국밥");
         autoCompleteTextList.add("돈앤밥");
         autoCompleteTextList.add("이디야");
+    }
+
+    /*필터 버튼 클릭하면 필터를 화면 위로 보여줌*/
+    protected void showFilter() {
+        filterView.setVisibility(View.VISIBLE);
+        filterView.bringToFront();
+    }
+
+    /*뒤로가기 버튼을 클릭하면 필터를 다시 숨김*/
+    protected void hideFilter() {
+        filterView.setVisibility(View.INVISIBLE);//Gone과 Invisible은 View가 보이지 않는다. 차이는 Invisible은 레이아웃을 위한 영역을 차지하고, Gone은 레이아웃을 위한 영역을 차지 안함
     }
 }
